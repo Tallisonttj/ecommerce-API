632c7278b1d4dd1b6cecdc1d45bdd8c4c4f4510b
Tallison Tadeu <tallisontadeu_037@hotmail.com>
2026-02-03 18:37:07 -0300

    update for gap bug

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -35,8 +35,8 @@ export default function Login () {
     <section className="">
       {!userCtx?.isLogged && 
       <div className="container flex items-center">
-        <div className="p-3 flex flex-col md:flex-row gap-4 items-center">
-          <div className="flex-1 h-120 p-3 flex flex-col  gap-4 text-left md:justify-between md:items-center md:max-w-xl">
+        <div className="p-3 flex flex-col md:flex-row gap-3 items-center">
+          <div className="flex-1 md:h-120 p-3 flex flex-col text-left md:justify-between md:items-center md:max-w-xl">
             <h1 className="p-3 text-3xl font-semibold">Já tenho um cadastro!</h1>
             <p className="text-xl">
               Insira seu e-mail e senha para acessar sua conta.
@@ -45,11 +45,13 @@ export default function Login () {
               <Input className="text-2xl"  label="E-mail" type="text" value={eMali} onclick={(e) => setEmail(e)} onCl={() => {}} />
             </div>
             
-            <div className="flex items-center h-full w-full p-2 md:max-w-md">
+            <div className="flex flex-col gap-2 items-center h-full w-full p-2 md:max-w-md">
               <Input className="text-2xl" value={senha} type={`${!mostrar? 'password' : 'text' }`} onclick={(e) => setSenha(e)} label="Senha" img={`/assets/images/${!mostrar ? 'icons8-olho-aberto30.png': 'icons8-olho-fechado30.png'}`} onCl={() => versenha()}/>
-            </div>
 
             <p className="cursor-pointer hover:underline">Esqueci a senha</p>
+
+            </div>
+
             <div className="text-center">
               <Button
                 msg="ENTRAR"
